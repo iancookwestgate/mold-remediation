097015f6666cf67aa04536977e7d32d94d677f7b
Student <example@example.com>
2019-03-07 11:24:11 -0800

    adjust colors for dots

--- src/index.js
+++ src/index.js
@@ -48,9 +48,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "grid";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
